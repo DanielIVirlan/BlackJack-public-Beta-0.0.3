83128b6255077bf139b79d21a39070715237dbf8
Daniel I.Virlan <virlan.1960677@studenti.uniroma1.it>
2024-07-17 22:45:08 +0200

    Delete docs directory

--- docs/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"model","l":"Card"},{"p":"controller","l":"GameLogic.CardImageData"},{"p":"model","l":"Deck"},{"p":"view","l":"GameGUI"},{"p":"controller","l":"GameLogic"},{"p":"view","l":"GameMenu"},{"p":"<Unnamed>","l":"JBlackJack"},{"p":"model","l":"Player"}];updateSearchResults();
